3a4c36e41d3ecc54a302adc52109633b1e3adda6
Daniele Veneroni <Venerons@users.noreply.github.com>
2022-11-14 12:06:39 +0100

    Update and rename quantum.js to quantum.utils.js

---
+++ src/quantum.utils.js
@@ -0,0 +1,123 @@
+// Quantum Utils
+// Copyright (c) 2017 – 2022 Daniele Veneroni. All rights reserved.
+// Licensed under the MIT License (X11 License)
+(function () {
+
+	var QuantumUtils = Object.create(null);
+
+	QuantumUtils.log = function () {
+		try {
+			const arg = Array.prototype.slice.call(arguments);
+			const level = arg.shift();
+			if (level === 'debug') {
+				arg.unshift('color: lightgreen');
+				arg.unshift('🔧 %c[Debug]');
+				console.log.apply(console, arg);
+			} else if (level === 'info') {
+				arg.unshift('color: dodgerblue');
+				arg.unshift('🔧 %c[Info]');
+				console.info.apply(console, arg);
+			} else if (level === 'warn') {
+				arg.unshift('color: orange');
+				arg.unshift('🔧 %c[Warning]');
+				console.warn.apply(console, arg);
+			} else if (level === 'error') {
+				arg.unshift('color: crimson');
+				arg.unshift('🔧 %c[Error]');
+				console.error.apply(console, arg);
+			} else {
+				arg.unshift(level);
+				console.log.apply(console, arg);
+			}
+		} catch (e) {
+			console.log(Array.prototype.slice.call(arguments));
+		}
+	};
+
+	QuantumUtils.error = function (error, message) {
+		QuantumUtils.log(
+			'error',
+			(message ? message : '') +
+			'\n\n' +
+			(error.toString()   ? 'Error:\t\t' + error.toString() : '') +
+			(error.name         ? '\nName:\t\t' + error.name : '') +
+			(error.message      ? '\nMessage:\t' + error.message : '') +
+			(error.fileName     ? '\nFile:\t\t' + error.fileName : '') +
+			(error.toSource     ? '\nSource:\t\t' + error.toSource() : '') +
+			(error.lineNumber   ? '\nLine #:\t\t' + error.lineNumber : '') +
+			(error.columnNumber ? '\nColumn #:\t' + error.columnNumber : '') +
+			(error.stack        ? '\n\nStack:\n\n' + error.stack : ''));
+	};
+
+	QuantumUtils.guid = function () {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+			const r = Math.random() * 16 | 0;
+			const v = c === 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		});
+	};
+
+	QuantumUtils.require = function (settings) {
+		let pending = settings.scripts.length;
+		const errors = [];
+		settings.scripts.forEach(function (script) {
+			if (script.type === 'js') {
+				const element = document.createElement('script');
+				element.type = 'application/javascript';
+				element.async = script.async ? true : false;
+				element.defer = script.defer ? true : false;
+				element.onerror = function (e) {
+					pending--;
+					if (script.onerror) {
+						script.onerror.call(null, e);
+					}
+					errors.push(e);
+					if (pending === 0 && settings.onload) {
+						settings.onload.call(null, errors);
+					}
+				};
+				element.onload = function () {
+					pending--;
+					if (script.onload) {
+						script.onload.call(null);
+					}
+					if (pending === 0 && settings.onload) {
+						settings.onload.call(null, errors);
+					}
+				};
+				element.src = script.path;
+				(document.body || document.head).appendChild(element);
+			} else if (script.type === 'css') {
+				const element = document.createElement('link');
+				element.rel = 'stylesheet';
+				element.type = 'text/css';
+				element.onerror = function (e) {
+					pending--;
+					if (script.onerror) {
+						script.onerror.call(null, e);
+					}
+					errors.push(e);
+					if (pending === 0 && settings.onload) {
+						settings.onload.call(null, errors);
+					}
+				};
+				element.onload = function () {
+					pending--;
+					if (script.onload) {
+						script.onload.call(null);
+					}
+					if (pending === 0 && settings.onload) {
+						settings.onload.call(null, errors);
+					}
+				};
+				element.href = script.path;
+				(document.head || document.body).appendChild(element);
+			}
+		});
+	};
+
+	if (!window.Quantum) {
+		window.Quantum = Object.create(null);
+	}
+	window.Quantum.utils = QuantumUtils;
+})();
